f6919b345167311835ab78052153a125849634c6
Varunkumar Manoharan <samvarun@gmail.com>
2024-09-01 16:10:23 +0530

    NextJs Food App setup

---
+++ components/images/image-picker.js
@@ -0,0 +1,60 @@
+'use client';
+import { useRef, useState } from 'react';
+import classes from './image-picker.module.css';
+import Image from 'next/image';
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setPickedImage(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
