8ef34019b76843dc4f46c5e57b92047dfb43b964
Varunkumar Manoharan <samvarun@gmail.com>
2024-09-07 17:18:01 +0530

    generate dynamic meta data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,6 +2,15 @@ import Image from 'next/image';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealsDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
   if (!meal) {
